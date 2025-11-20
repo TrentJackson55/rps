8a704b54c4739e345f2dc8945d0ceb421a0051c3
TrentJackson55 <TrentJackson55@users.noreply.github.com>
2025-11-19 23:44:19 -0800

    Implement human choice and play round function

--- rps.js
+++ rps.js
@@ -1,31 +1,71 @@
 // Get computer choice (function)
 // return rock, paper or scissors randomly
 // Math.random
+// Create and initialize (0) score variables, comp and player
+
+let humanScore = 0;
+let compScore = 0;
+
 function getCompChoice() {
-    let choice = "";
+    let compChoice = "";
     let selection = Math.random();
     
     if (selection < .34) {
-        choice = "rock"
+        compChoice = "rock"
     } else if (selection < .67 && selection > .34) {
-        choice = "paper"
+        compChoice = "paper"
     } else {
-        choice = "scissors"
+        compChoice = "scissors"
     }
-    return choice;
+    return compChoice;
 }
 
 // Get human choice (function)
 // Use prompt method, store to variable, return
-
-// Create and initialize (0) score variables, comp and player
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper, or Scissors? : ");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
+}
 
 // Write playRound function
 // two parameters, humanChoice and compChoice
 // make humanChoice case-insensitive (toLowerCase?)
 // example output: "You lose! Papper beats Rock"
 // increment either humanScore or compScore
+function playRound(humanChoice, compChoice) {
+    //Rock outcomes
+    if (humanChoice === "rock" && compChoice === "rock") {
+        console.log("Draw! No points awarded");
+    } else if (humanChoice === "rock" && compChoice === "paper") {
+        console.log("You lose! Paper beats Rock.");
+        compScore++;
+    } else if (humanChoice === "rock" && compChoice === "scissors") {
+        console.log("You win! Rock beats Scissors.");
+        humanScore++;    
+    } else if (humanChoice === "paper" && compChoice === "rock") {
+        console.log("You win! Paper beats Rock.");
+        humanScore++;
+    } else if (humanChoice === "paper" && compChoice === "paper") {
+        console.log("Draw! No points awarded");
+    } else if (humanChoice === "paper" && compChoice === "scissors") {
+        console.log("You lose! Scissors beat Paper.");
+        compScore++;
+    } else if (humanChoice === "scissors" && compChoice === "rock") {
+        console.log("You lose! Rock beats Scissors.");
+        compScore++;
+    } else if (humanChoice === "scissors" && compChoice === "paper") {
+        console.log("You win! Scissors beat Paper.");
+        humanScore++;
+    } else if (humanChoice === "scissors" && compChoice === "scissors") {
+        console.log("Draw! No points awarded");
+    }
+}
 
+const humanSelection = getHumanChoice();
+const compSelection = getCompChoice();
+
+playRound(humanSelection, compSelection);
 // Write playGame (best of 5)
 // Move playRound and score variables so they are declared in
 // playGame
